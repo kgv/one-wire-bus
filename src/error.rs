8bfe0cd4fd5f686cb3ef27dd69e8287d818cf4c2
g <kgv@users.noreply.github.com>
2020-11-20 03:58:05 -0800

    Update error.rs

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,4 @@
-use core::fmt::Debug;
+use core::fmt::{self, Debug, Display, Formatter};
 
 pub type OneWireResult<T, E> = Result<T, OneWireError<E>>;
 
@@ -16,4 +16,17 @@ pub enum OneWireError<E> {
     FamilyCodeMismatch,
     CrcMismatch,
     Timeout,
+}
+
+impl<E: Display> Display for OneWireError<E> {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        match self {
+            Self::BusNotHigh => write!(f, "The Bus was expected to be pulled high by a ~5K ohm pull-up resistor, but it wasn't."),
+            Self::PinError(error) => write!(f, "Pin error: {}.", error),
+            Self::UnexpectedResponse => write!(f, "An unexpected response was received from a command."),
+            Self::FamilyCodeMismatch => write!(f, "Family code mismatch."),
+            Self::CrcMismatch => write!(f, "CRC mismatch."),
+            Self::Timeout => write!(f, "Timeout."),
+        }
+    }
 }
